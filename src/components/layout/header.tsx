037874084d051aeaab66edb97ae1afb84ea5e42d
cleggacus <cleggacus@gmail.com>
2025-05-03 12:58:30 +0100

    header logo
    added a logo image to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,13 +3,20 @@
 import { useAtTop } from "@/hooks/atTop";
 import styles from "./header.module.css"
 import Nav from "./nav"
+import Image from "next/image";
+import logoImage from "../../../public/logo.png";
 
 export function Header() {
   const atTop = useAtTop();
 
   return <div className={`${styles.container} ${atTop ? styles.atTop : ""}`}>
     <div className={styles.left}>
-      <h1>Some Logo</h1>
+      <Image
+        src={logoImage}
+        alt="Company logo"
+        className={styles.logo}
+      />
+      <h1>ision Connect Zero</h1>
     </div>
 
     <div className={styles.right}>
