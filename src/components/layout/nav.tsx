6c5d8a32623c663431b1d29fe46b322bcfb5f533
cleggacus <cleggacus@gmail.com>
2025-05-03 02:59:07 +0100

    responsive nav

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,21 +1,51 @@
+"use client";
+
+import { IoMenu } from "react-icons/io5";
 import styles from "./nav.module.css";
 import Link from "next/link";
+import { useEffect, useRef, useState } from "react";
 
 export default function Nav() {
-  return <div className={styles.container}>
-    <Link
-      href="#hero"
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setOpen(false);
+    }
+  }
 
-      scroll
-    >Hero</Link>
+  const handleScroll = () => {
+    setOpen(false);
+  }
 
-    <Link href="#about" scroll={true}>About Us</Link>
+  useEffect(() => {
+    if (open) {
+      document.addEventListener("mousedown", handleClickOutside);
+      window.addEventListener("scroll", handleScroll);
+    }
 
-    <Link href="#services" scroll={true}>Services</Link>
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [open]);
 
-    <Link href="#projects" scroll={true}>Projects</Link>
+  return <div
+    className={styles.container}
+    ref={ref}
+  >
+    <IoMenu
+      onClick={() => setOpen(open => !open)}
+      className={styles.menuIcon}
+    />
 
-    <Link href="#contact" scroll={true}>Contact</Link>
-  </div>
+    <div className={`${styles.links} ${open ? styles.open : styles.close}`}>
+      <Link href="#hero" scroll={true}>Hero</Link>
+      <Link href="#about" scroll={true}>About Us</Link>
+      <Link href="#services" scroll={true}>Services</Link>
+      <Link href="#projects" scroll={true}>Projects</Link>
+      <Link href="#contact" scroll={true}>Contact</Link>
+    </div>
+  </div >
 }
